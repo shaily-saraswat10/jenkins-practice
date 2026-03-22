6fdcd525c97cb5053561a864d5db868ac1d25d01
shaily-saraswat10 <2024pgcsca023@nitjsr.ac.in>
2026-03-22 19:50:48 +0530

    Update UserList.js

--- client/src/containers/Users/UserList.js
+++ client/src/containers/Users/UserList.js
@@ -3,7 +3,9 @@ import React from 'react';
 import Typography from '@mui/material/Typography';
 
 const UserList = () => {
-  return <Typography>UserList Page</Typography>;
+  return <div><Typography>UserList Page</Typography>;
+  <h1>hello</hello>
+    </div>
 };
 
 export default UserList;
